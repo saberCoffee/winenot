f807cf8e8fd137f74bf93f84fe7633b617b8c834
novlike <novlike@gmail.com>
2017-02-04 18:42:40 +0100

    Google Map API dans le home page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,6 @@
-// FONCTION CAROUSEL
-$(document).ready(function()
- {
+
+$(function() {
+	
     $('#Carousel').carousel({
         interval: 5000
     })
@@ -36,4 +36,52 @@ $(document).ready(function()
             $("#goto-top").fadeOut();
         }
     });
+    
+    
+    /*
+		Script pour google map API dans le homepage
+     */
+    
+    function initialize() {
+
+    	  var styleArray = [
+    	    {
+    	      featureType: 'all',
+    	      stylers: [
+    	        { saturation: -80 }
+    	      ]
+    	    },{
+    	      featureType: 'road.arterial',
+    	      elementType: 'geometry',
+    	      stylers: [
+    	        { hue: '#00ffee' },
+    	        { saturation: 50 }
+    	      ]
+    	    },{
+    	      featureType: 'poi.business',
+    	      elementType: 'labels',
+    	      stylers: [
+    	        { visibility: 'off' }
+    	      ]
+    	    }
+    	  ];
+
+    	  var mapOptions = {
+    	    zoom: 6,
+    	    center: new google.maps.LatLng(47.081012, 2.398781999999983),
+    	    styles: styleArray,
+    	    scrollwheel: false
+    	  };
+
+    	  var map = new google.maps.Map(document.getElementById('map'),
+    	    mapOptions);
+    	}
+
+    	google.maps.event.addDomListener(window, 'load', initialize);
+    
+    /*
+      Fin de Script pour google map API 
+     */
+    	
+   
 });
