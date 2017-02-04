f47c1fb023da109b479df480d99f18fa15904453
Tadeca <th.mion@gmail.com>
2017-02-04 17:10:27 +0100

    Séparation du fichier .css en plusieurs feuilles pour chaque dev

---
+++ app/prod/includes/googlemap.php
@@ -0,0 +1,18 @@
+
+<section id="GoogleMap">
+	<div id="WinmakerSearch">
+		<h2>Où trouver nos 500 producteurs</h2>
+		<p>Trouver un producteur près de chez vous</p>
+		<form>
+            <div class="input-group stylish-input-group">
+                <input type="text" class="form-control"  placeholder="Trouver un producteur" >
+                <span class="input-group-addon">
+                    <button type="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+			</div>
+		</form>
+	</div>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10502.476747112658!2d2.3324169500000003!3d48.8464021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1486026989091" width="100%" height="700px" frameborder="0" style="border:0" allowfullscreen></iframe>
+</section>
