10df7b07886064d64e5cf7b62bf4bb5db05c246b
IRONBAN <heramban_minatchy@hotmail.com>
2017-02-05 17:41:47 +0100

    Maj de la section producteur du mois

--- app/prod/includes/producteurs.php
+++ app/prod/includes/producteurs.php
@@ -2,82 +2,165 @@
 	<h2>Producteurs du mois</h2>
 
 <div class="container">
+        <div id="main_area">
+                <!-- Slider -->
+                <div class="row">
+                    <div class="col-sm-12" id="slider">
+                        <!-- Top part of the slider -->
+                        <div class="row">
+                            <div class="col-sm-4" id="carousel-bounding-box" >
+                                <div class="carousel slide" id="myCarousel" >
+                                    <!-- Carousel items -->
+                                    <div class="carousel-inner" >
+                                        <div class="active item" data-slide-number="0" >
+                                        <img src="assets/img/pierre.jpg" width="100%"></div>
 
-    <div class="row">
-		<div class="col-md-12">
-                <div id="Carousel" class="carousel slide">
+                                        <div class="item" data-slide-number="1">
+                                        <img src="assets/img/moi.jpg" width="100%"></div>
 
-                <!-- <ol class="carousel-indicators">
-                    <li data-target="#Carousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#Carousel" data-slide-to="1"></li>
-                    <li data-target="#Carousel" data-slide-to="2"></li>
-                </ol> -->
+                                        <div class="item" data-slide-number="2">
+                                        <img src="assets/img/thomas.jpg" width="100%"></div>
 
+                                        <div class="item" data-slide-number="3">
+                                        <img src="assets/img/romain.jpg" width="100%"></div>
 
-                <!-- Carousel items -->
-                <div class="carousel-inner">
+                                        <div class="item" data-slide-number="4">
+                                        <img src="assets/img/hwaseon.jpg" width="100%"></div>
 
-                <div class="item active">
-                	<div class="row">
-                	  <div class="col-md-3">
-                	  <a href="yann" class="thumbnail">
-                	  <div class="carousel-caption">
-		                    <h3>Yann Le Merrer</h3>
-		                    <p>Producteur à Nanterre</p>
-		              </div>
-		              <img src="assets/img/yann.jpg" alt="Image" style="max-width:100%;"></a>
-                	  </div>
+                                        <div class="item" data-slide-number="5">
+                                        <img src="assets/img/yann.jpg" width="100%"></div>
 
-                	  <div class="col-md-3"><a href="hwaseon" class="thumbnail">
-                	  <div class="carousel-caption">
-		                    <h3>Hwa Seon</h3>
-		                    <p>Producteur à Séoul</p>
-		              </div><img src="assets/img/hwaseon.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	  <div class="col-md-3"><a href="romain" class="thumbnail">
-                	  <div class="carousel-caption">
-		                    <h3>Romain Hamel</h3>
-		                    <p>Producteur chez lui</p>
-		              </div><img src="assets/img/romain.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	  <div class="col-md-3"><a href="thomas" class="thumbnail">
-                	  <div class="carousel-caption">
-		                    <h3>Thomas Mion</h3>
-		                    <p>Producteur à Vincennes</p>
-		              </div><img src="assets/img/thomas.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	</div><!--.row-->
-                </div><!--.item-->
+                                        <div class="item" data-slide-number="5">
+                                        <img src="assets/img/yann.jpg" width="100%"></div>
 
-                <div class="item">
-                	<div class="row">
-                		<div class="col-md-3"><a href="pierre" class="thumbnail">
-                		<div class="carousel-caption">
-		                    <h3>Pierre Vaneste</h3>
-		                    <p>Producteur à Convention</p>
-		              </div><img src="assets/img/pierre.jpg" alt="Image" style="max-width:100%;"></a></div>
-                		<div class="col-md-3"><a href="thibaut" class="thumbnail">
-                		<div class="carousel-caption">
-		                    <h3>Thibaut Molitor</h3>
-		                    <p>Producteur à la Fnac</p>
-		              </div><img src="assets/img/thibaut.jpg" alt="Image" style="max-width:100%;"></a></div>
-                		<div class="col-md-3"><a href="moi" class="thumbnail">
-                		<div class="carousel-caption">
-		                    <h3>Héramban Minatchy</h3>
-		                    <p>Producteur à La Réunion</p>
-		              </div><img src="assets/img/moi.jpg" alt="Image" style="max-width:100%;"></a></div>
-                		<div class="col-md-3"><a href="yann" class="thumbnail">
-                		<div class="carousel-caption">
-		                    <h3>Yann Le Merrer</h3>
-		                    <p>Producteur à Nanterre</p>
-		              </div><img src="assets/img/yann.jpg" alt="Image" style="max-width:100%;"></a></div>
-                	</div><!--.row-->
-                </div><!--.item-->
+                                        <div class="item" data-slide-number="5">
+                                        <img src="assets/img/yann.jpg" width="100%"></div>
 
+                                    </div><!-- Carousel nav -->
+                                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                                        <span class="glyphicon glyphicon-chevron-left"></span>                                       
+                                    </a>
+                                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                                        <span class="glyphicon glyphicon-chevron-right"></span>                                       
+                                    </a>                                
+                                    </div>
+                            </div>
 
-                </div><!--.carousel-inner-->
-                  <a data-slide="prev" href="#Carousel" class="left carousel-control">‹</a>
-                  <a data-slide="next" href="#Carousel" class="right carousel-control">›</a>
-                </div><!--.Carousel-->
+                            <div class="col-sm-4" id="carousel-text"></div>
 
-		</div>
-	</div>
-</div><!--.container-->
+                            <div id="slide-content" style="display: none;">
+                                <div id="slide-content-0">
+                                    <h2>Pierre Vaneste</h2>
+                                    <p>Producteur à Convention et sur le dancefloor</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+                                </div>
+
+                                <div id="slide-content-1">
+                                    <h2>Héramban Minatchy</h2>
+                                    <p>Producteur à La Réunion</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+                                </div>
+
+                                <div id="slide-content-2">
+                                    <h2>Thomas Mion</h2>
+                                    <p>Producteur à Vincennes</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+                                </div>
+
+                                <div id="slide-content-3">
+                                    <h2>Romain Hamel</h2>
+                                    <p>Producteur chez lui</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+                                </div>
+
+                                <div id="slide-content-4">
+                                    <h2>Hwa Seon Lecouey</h2>
+                                    <p>Producteur à Séoul</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+                                </div>
+
+                                <div id="slide-content-5">
+                                    <h2>Yann Le Merrer</h2>
+                                    <p>Producteur à la Retraite</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--/Slider-->
+
+                <div class="row hidden-xs" id="slider-thumbs">
+                        <!-- Bottom switcher of slider -->
+                        <ul class="hide-bullets">
+                            <li class="col-sm-2">
+                                <a class="thumbnail" id="carousel-selector-0"><img src="assets/img/pierre.jpg" ></a>
+                            </li>
+
+                            <li class="col-sm-2">
+                                <a class="thumbnail" id="carousel-selector-1"><img src="assets/img/moi.jpg" ></a>
+                            </li>
+
+                            <li class="col-sm-2">
+                                <a class="thumbnail" id="carousel-selector-2"><img src="assets/img/thomas.jpg" ></a>
+                            </li>
+
+                            <li class="col-sm-2">
+                                <a class="thumbnail" id="carousel-selector-3"><img src="assets/img/romain.jpg" ></a>
+                            </li>
+
+                            <li class="col-sm-2">
+                                <a class="thumbnail" id="carousel-selector-4"><img src="assets/img/hwaseon.jpg" ></a>
+                            </li>
+
+                            <li class="col-sm-2">
+                                <a class="thumbnail" id="carousel-selector-5"><img src="assets/img/yann.jpg" ></a>
+                            </li>
+
+                            <li class="col-sm-2">
+                                <a class="thumbnail" id="carousel-selector-5"><img src="assets/img/yann.jpg" ></a>
+                            </li>
+
+                            <li class="col-sm-2">
+                                <a class="thumbnail" id="carousel-selector-5"><img src="assets/img/yann.jpg" ></a>
+                            </li>
+                        </ul>                 
+                </div>
+        </div>
+</div>
 </section>
