250fb2897603a383df9943fd0ce0a3112abcd809
Tadeca <th.mion@gmail.com>
2017-02-05 14:15:06 +0100

    (Temporaire) Transfert de "Producteurs du mois" de la homepage à un fichier include pour éviter les conflits: )

---
+++ app/prod/includes/producteurs.php
@@ -0,0 +1,83 @@
+<section id="ProductMonth">
+	<h2>Producteurs du mois</h2>
+
+<div class="container">
+
+    <div class="row">
+		<div class="col-md-12">
+                <div id="Carousel" class="carousel slide">
+
+                <!-- <ol class="carousel-indicators">
+                    <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#Carousel" data-slide-to="1"></li>
+                    <li data-target="#Carousel" data-slide-to="2"></li>
+                </ol> -->
+
+
+                <!-- Carousel items -->
+                <div class="carousel-inner">
+
+                <div class="item active">
+                	<div class="row">
+                	  <div class="col-md-3">
+                	  <a href="yann" class="thumbnail">
+                	  <div class="carousel-caption">
+		                    <h3>Yann Le Merrer</h3>
+		                    <p>Producteur à Nanterre</p>
+		              </div>
+		              <img src="assets/img/yann.jpg" alt="Image" style="max-width:100%;"></a>
+                	  </div>
+
+                	  <div class="col-md-3"><a href="hwaseon" class="thumbnail">
+                	  <div class="carousel-caption">
+		                    <h3>Hwa Seon</h3>
+		                    <p>Producteur à Séoul</p>
+		              </div><img src="assets/img/hwaseon.jpg" alt="Image" style="max-width:100%;"></a></div>
+                	  <div class="col-md-3"><a href="romain" class="thumbnail">
+                	  <div class="carousel-caption">
+		                    <h3>Romain Hamel</h3>
+		                    <p>Producteur chez lui</p>
+		              </div><img src="assets/img/romain.jpg" alt="Image" style="max-width:100%;"></a></div>
+                	  <div class="col-md-3"><a href="thomas" class="thumbnail">
+                	  <div class="carousel-caption">
+		                    <h3>Thomas Mion</h3>
+		                    <p>Producteur à Vincennes</p>
+		              </div><img src="assets/img/thomas.jpg" alt="Image" style="max-width:100%;"></a></div>
+                	</div><!--.row-->
+                </div><!--.item-->
+
+                <div class="item">
+                	<div class="row">
+                		<div class="col-md-3"><a href="pierre" class="thumbnail">
+                		<div class="carousel-caption">
+		                    <h3>Pierre Vaneste</h3>
+		                    <p>Producteur à Convention</p>
+		              </div><img src="assets/img/pierre.jpg" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="thibaut" class="thumbnail">
+                		<div class="carousel-caption">
+		                    <h3>Thibaut Molitor</h3>
+		                    <p>Producteur à la Fnac</p>
+		              </div><img src="assets/img/thibaut.jpg" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="moi" class="thumbnail">
+                		<div class="carousel-caption">
+		                    <h3>Héramban Minatchy</h3>
+		                    <p>Producteur à La Réunion</p>
+		              </div><img src="assets/img/moi.jpg" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="yann" class="thumbnail">
+                		<div class="carousel-caption">
+		                    <h3>Yann Le Merrer</h3>
+		                    <p>Producteur à Nanterre</p>
+		              </div><img src="assets/img/yann.jpg" alt="Image" style="max-width:100%;"></a></div>
+                	</div><!--.row-->
+                </div><!--.item-->
+
+
+                </div><!--.carousel-inner-->
+                  <a data-slide="prev" href="#Carousel" class="left carousel-control">‹</a>
+                  <a data-slide="next" href="#Carousel" class="right carousel-control">›</a>
+                </div><!--.Carousel-->
+
+		</div>
+	</div>
+</div><!--.container-->
+</section>
