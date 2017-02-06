1340e64a6efb011b5d96875f03de1366c8b06c75
Tadeca <th.mion@gmail.com>
2017-02-06 18:00:24 +0100

    Modifications mineures du layout du dashboard

--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -1,15 +1,31 @@
-<?php $this->layout('layout_dashboard', ['title' => 'Espace membre']) ?>
+<?php $this->layout('layout_dashboard', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-<h1>WineNot<span>Accueil</span></h1>
-
 <article>
-    <h2>Les producteurs de WineNot</h2>
+    <h2>Nos producteurs</h2>
 
     <img src="<?= $this->assetUrl('img/dashboard/home-winemakers.jpg') ?>" alt="Producteurs" class="img-responsive" />
 
     <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+    <p>
+        <a href="#">Découvrez nos producteurs</a>
+    </p>
+</article>
+
+<article>
+    <h2>Nos produits</h2>
+
+    <img src="<?= $this->assetUrl('img/dashboard/home-winemakers.jpg') ?>" alt="Producteurs" class="img-responsive" />
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+    <p>
+        <a href="#">Découvrez nos produits</a>
     </p>
 </article>
 <?php $this->stop('main_content') ?>
