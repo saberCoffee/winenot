8e56532bb37ae894fcb555b20e9db24e7599ccb2
TADECA-PC\Tadeca <th.mion@gmail.com>
2017-02-06 23:09:32 +0100

    Correction JS sur le dashboard (le popup "Mon compte" jouait à la guirlande de noël à s'ouvrir et se fermer n'importe quand...) + Inclusion jQuery UI sur le dashboard

--- app/Views/layout_dashboard.php
+++ app/Views/layout_dashboard.php
@@ -14,6 +14,8 @@
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/dashboard/style.css') ?>" />
 
 	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+	<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+
 </head>
 
 <body class="dashboard">
--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,12 +1,15 @@
 $(function() {
     //-- Start : Popup "Mon compte" du dashboard --//
     function popupMyAccount() {
-        $('.open-account-popup').on('click', function() {
-            $(this).next('.account-popup').show();
+        $('.open-account-popup').on('click', function(event) {
+            event.stopPropagation(); // Empêche d'afficher/cacher le popup accidentellement en cliquant sur le body en même temps que le lien
+            event.preventDefault();
+
+            $(this).next('.account-popup').toggle('blind');
         });
 
         $('body').on('click', function() {
-            $('.account-popup').toggle();
+            $('.account-popup').hide('blind');
         });
     }
     //-- End : Popup "Mon compte" du dashboard --//
