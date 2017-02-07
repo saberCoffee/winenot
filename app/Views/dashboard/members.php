7499e6094823cd850ac1b693e8b88c0bc8994085
novlike <novlike@gmail.com>
2017-02-07 17:03:28 +0100

    Front de la gestion des membres et des producteurs

--- app/Views/dashboard/members.php
+++ app/Views/dashboard/members.php
@@ -1,38 +1,61 @@
 <?php $this->layout('layout_dashboard', ['title' => 'Tous nos membres']) ?>
 <?php $this->start('main_content') ?>
 
-<div class="container-fluid">
-	
+
+
+<div class="container">
+	<h2 class="members">Ajouter un utilisateur</h2>
 	<form action="" method="post" class="members">
-		<h2>Ajouter un utilisateur</h2>
+
 		<input type="hidden" name="id" value="<?= $members['id']; ?>" />
-		<div class="form-group">
-		<label for="">Prénom</label>
-		<input type="text" value="" name="firstnsame" placeholder="prénom" class="form-control" />
-		<label for="">Nom</label>
-		<input type="text" value="" name="lastname" placeholder="nom"  class="form-control"/>
-		<label for="">Email</label>
-		<input type="email" value="" name="email" placeholder="email"  class="form-control" />
+			<div class="row">
+				<div class="col-md-4">
+					<label for="">Prénom</label>
+					<input type="text" value="" name="firstnsame" placeholder="prénom" class="form-control" />
+				</div>
+				<div class="col-md-4">
+					<label for="">Nom</label>
+					<input type="text" value="" name="lastname" placeholder="nom"  class="form-control"/>
+				</div>
+				<div class="col-md-4">
+					<label for="">Email</label>
+					<input type="email" value="" name="email" placeholder="email"  class="form-control" />
+				</div>
+			</div>
+		<div class="row">
+				<div class="col-md-4">
+					<label for="">Adresse</label>
+					<input type="text" value="" name="adress" placeholder="adresse" class="form-control" />
+				</div>
+				<div class="col-md-4">
+					<label for="">Ville</label>
+					<input type="text" value="" name="city" placeholder="ville" class="form-control" />
+				</div>
+				<div class="col-md-4">
+				<label for="">Code Postale</label>
+				<input type="number" value="" name="postcode" placeholder="code postale" class="form-control" />
+				</div>
+			</div>
+		<div class="row">
+			<div class="col-md-6">
+				<label for="">Role</label>
+				<input type="number" value="" name="role" placeholder="admin ou membre"  class="form-control"/>
+			</div>
+			<div class="col-md-6">
+				<label for="">Type</label>
+				<input type="number" value="" name="type" placeholder="producteur ou membre" class="form-control" />
+			</div>
 		</div>
-		<div class="form-group">
-		<label for="">Adresse</label>
-		<input type="text" value="" name="adress" placeholder="adresse" class="form-control" />
-		<label for="">Ville</label>
-		<input type="text" value="" name="city" placeholder="ville" class="form-control" />
-		<label for="">Code Postale</label>
-		<input type="number" value="" name="postcode" placeholder="code postale" class="form-control" />
+		<div class="row">
+		<div class="col-md-12">
+			<input type="submit" value="ajouter"  class="btn btn-default"/>
 		</div>
-		<div class="form-group">
-		<label for="">Role</label>
-		<input type="number" value="" name="role" placeholder="admin ou membre"  class="form-control"/>
-		<label for="">Type</label>
-		<input type="number" value="" name="type" placeholder="producteur ou membre" class="form-control" />
-		<input type="submit" value="ajouter"  class="btn btn-default"/>
 		</div>
 	</form>
-		
+	
 	<h2 class="members">Liste des membres</h2>
-	<table class="table table-bordered">
+	<section class="member-list">
+	<table border="1" class="table table-striped">
 		<tr>
 			<th>Prénom</th>
 			<th>Nom</th>
@@ -42,7 +65,7 @@
 			<th>Code Postale</th>
 			<th>Role</th>
 			<th>Type</th>
-			<th colspan="3">Action</th>
+			<th colspan="3"></th>
 		</tr>
 	<?php foreach ($members as $member) : ?>
 		<tr>
@@ -54,9 +77,16 @@
 			<td><?= $member['postcode']?></td>
 			<td><?= $member['role']?></td>
 			<td><?= $member['type']?></td>
-			<td class="action"><a href="<?= $this->url('members', ['id' => $member['id']]) ?>" style="color:black"><i class="fa fa-user" aria-hidden="true"></i></a> | <a href="<?= $this->url('members2', ['id' => $member['id']]) ?>" style="color:green"><i class="fa fa-pencil" aria-hidden="true"></i></a> | <a href="<?= $this->url('members', ['id' => $member['id']]) ?>" style="color:red"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
-			</tr>
-	<?php endforeach;?>
+			<td class="action"><!--
+			--><a href="<?= $this->url('members', ['id' => $member['id']]) ?>" style="color:black"><i class="fa fa-user" aria-hidden="true"></i></a><!--
+			--><a href="<?= $this->url('members2', ['id' => $member['id']]) ?>"><img width="20" src="<?= $this->assetUrl('img/dashboard/edit.png') ?>" alt="modifier"></a><!--
+			--><a href="<?= $this->url('members', ['id' => $member['id']]) ?>"><img width="20" src="<?= $this->assetUrl('img/dashboard/delete.png') ?>" alt="supprimer"></a><!--
+			--></td>
+					</tr>
+			<?php endforeach;?>
+
+	</table>
+	</section>
 </div>
 
 
--- app/Views/dashboard/winemakers.php
+++ app/Views/dashboard/winemakers.php
@@ -1,34 +1,48 @@
 <?php $this->layout('layout_dashboard', ['title' => 'Tous nos producteurs']) ?>
 <?php $this->start('main_content') ?>
 
-<div class="container-fluid">
-	
-	<form action="" method="post" class="members">
-		<h2>Ajouter un producteur</h2>
+<div class="container">
+	<h2 class="winemakers">Ajouter un producteur</h2>
+	<form action="" method="post" class="winemakers">
 		<input type="hidden" name="id" value="<?= $members['id']; ?>" />
-		<div class="form-group">
-			<label for="">Siren</label>
-			<input type="number" value="" name="siren" placeholder="sinen" class="form-control" />
-			<label for="">domain</label>
-			<input type="text" value="" name="domain" placeholder="domain"  class="form-control"/>
+		<div class="row">
+			<div class="col-md-4">
+				<label for="">Adresse</label>
+				<input type="text" value="" name="adress" placeholder="adresse" class="form-control" />
+			</div>
+			<div class="col-md-4">
+				<label for="">Ville</label>
+				<input type="text" value="" name="city" placeholder="ville" class="form-control" />
+			</div>
+			<div class="col-md-4">
+				<label for="">Code Postale</label>
+				<input type="number" value="" name="postcode" placeholder="code postale" class="form-control" />
+			</div>
 		</div>
-		<div class="form-group">
-		<label for="">Adresse</label>
-		<input type="text" value="" name="adress" placeholder="adresse" class="form-control" />
-		<label for="">Ville</label>
-		<input type="text" value="" name="city" placeholder="ville" class="form-control" />
-				</div>
-		<div class="form-group">
-		<label for="">Code Postale</label>
-		<input type="number" value="" name="postcode" placeholder="code postale" class="form-control" />
-		<label for="">N° Téléphone</label>
-		<input type="number" value="" name="tel" placeholder="numéro de téléphone" class="form-control" />
-		<input type="submit" value="ajouter"  class="btn btn-default"/>
+		<div class="row">
+			<div class="col-md-4">
+				<label for="">Siren</label>
+				<input type="number" value="" name="siren" placeholder="sinen" class="form-control" />
+			</div>
+			<div class="col-md-4">
+				<label for="">domain</label>
+				<input type="text" value="" name="domain" placeholder="domain"  class="form-control"/>
+			</div>
+			<div class="col-md-4">
+				<label for="">N° Téléphone</label>
+				<input type="number" value="" name="tel" placeholder="numéro de téléphone" class="form-control" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<input type="submit" value="ajouter"  class="btn btn-default"/>
+			</div>
 		</div>
 	</form>
 		
 	<h2 class="members">Liste des producteurs</h2>
-	<table class="table table-bordered">
+	<section class="member-list">
+	<table border="1" class="table table-striped">
 		<tr>
 			<th>Siren</th>
 			<th>Domain</th>
@@ -36,7 +50,7 @@
 			<th>Viille</th>
 			<th>Code Postale</th>
 			<th>N° Téléphone</th>
-			<th colspan="3">Action</th>
+			<th colspan="3"></th>
 		</tr>
 	<?php foreach ($winemakers as $winemaker) : ?>
 		<tr>
@@ -46,9 +60,15 @@
 			<td><?= $winemaker['city'];?></td>
 			<td><?= $winemaker['postcode']?></td>
 			<td><?= $winemaker['tel']?></td>
-			<td class="action"><a href="<?= $this->url('winemakers', ['id' => $member['winemaker_id']]) ?>" style="color:black"><i class="fa fa-user" aria-hidden="true"></i></a> |<a href="<?= $this->url('winemakers', ['id' => $winemaker['winemakers_id']]) ?>" style="color:green"><i class="fa fa-pencil" aria-hidden="true"></i></a> | <a href="<?= $this->url('winemakers', ['id' => $winemaker['winemaker_id']]) ?>" style="color:red"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
-			</tr>
+			<td class="action"><!--
+			 --><a href="<?= $this->url('winemakers', ['id' => $winemaker['winemakers_id']]) ?>" style="color:black"><i class="fa fa-user" aria-hidden="true"></i></a><!--
+			 --><a href="<?= $this->url('winemakers', ['id' => $winemaker['winemakers_id']]) ?>"><img width="20" src="<?= $this->assetUrl('img/dashboard/edit.png') ?>" alt="modifier"></a><!--
+			 --><a href="<?= $this->url('winemakers', ['id' => $winemaker['winemakers_id']]) ?>"><img width="20" src="<?= $this->assetUrl('img/dashboard/delete.png') ?>" alt="supprimer"></a>
+			</td>
+		</tr>
 	<?php endforeach;?>
+	</table>
+	</section>
 </div>
 
 
