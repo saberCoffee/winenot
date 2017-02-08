dc9ac64247706d1f25f3ad5fa1a243e915246cea
novlike <novlike@gmail.com>
2017-02-08 18:58:24 +0100

    Ajout du favicon, ouaisssssss !!!!!!!!!!!!

--- app/Views/layout_dashboard.php
+++ app/Views/layout_dashboard.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
 
 	<title><?= $this->e($title) . ' - ' . $w_site_name ?></title>
-
+	<link rel="icon" href="<?= $this->assetUrl('img/icon.ico') ?>" />
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.min.css') ?>" />
 	<!-- Font Awesome -->
