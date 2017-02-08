8a913de15d9c259c919b5c2a2959367e946d1507
Romain-Hamel <romainhamel3d@gmail.com>
2017-02-08 14:50:51 +0100

    travail sur l'ajout de produit du dashboard

--- app/Views/dashboard/cave.php
+++ app/Views/dashboard/cave.php
@@ -10,9 +10,9 @@
 		</ul>
 	<section class="addProduct active">
 		<form method="POST">
-			<div class="form-group">
+			<div class="form-group <?php if (isset($error['name'])) { echo 'has-error'; } ?>">
 						<label for="product">Nom du produit</label>
-						<input type="text" name="name" class="form-control" value="<?= $name; ?>">
+						<input type="text" name="name" id="name" class="form-control" value="<?= $name; ?>" data-min="3" data-max="16" required="required">
 						<span class="help-block" <?php if (empty($error['name'])) { echo 'style="display: none"'; } ?>>
                         <?php if (isset($error['name'])) { echo $error['name']; } ?>
                    		 </span>
@@ -20,9 +20,9 @@
 	
 			<div class="row">
 				<div class="col-md-4">
-					<div class="form-group">
+					<div class="form-group <?php if (isset($error['color'])) { echo 'has-error'; } ?>">
 						<label for="color">Couleur</label>
-						<select name="color" class="form-control" value="<?= $color; ?>">
+						<select name="color" id="color" class="form-control" value="<?= $color; ?>" required="required">
 							<option value="">-- Selectionner --</option>
 							<option value="Blanc" <?php if ($color == 'Blanc') echo 'selected' ?>>Blanc</option>
 							<option value="Rosé"  <?php if ($color == 'Rosé') echo 'selected' ?>>Rosé</option>
@@ -35,9 +35,9 @@
 				</div>
 
 				<div class="col-md-4 ">
-					<div class="form-group">
+					<div class="form-group <?php if (isset($error['price'])) { echo 'has-error'; } ?>">
 						<label for="price">Prix (en euro)</label>
-						<input type="text" name="price" class="form-control" value="<?= $price; ?>">
+						<input type="text" name="price" id="price" class="form-control" value="<?= $price; ?>" required="required">
 						<span class="help-block" <?php if (empty($error['price'])) { echo 'style="display: none"'; } ?>>
                         <?php if (isset($error['price'])) { echo $error['price']; } ?>
                    		 </span>
@@ -45,9 +45,9 @@
 				</div>
 
 				<div class="col-md-4 ">
-					<div class="form-group">
+					<div class="form-group <?php if (isset($error['millesime'])) { echo 'has-error'; } ?>">
 						<label for="millesime">Millesime</label>
-						<input type="text" name="millesime" class="form-control" value="<?= $millesime; ?>">
+						<input type="text" name="millesime" id="millesime" class="form-control" value="<?= $millesime; ?>" data-min="4" data-max="4" required="required">
 						<span class="help-block" <?php if (empty($error['millesime'])) { echo 'style="display: none"'; } ?>>
                         <?php if (isset($error['millesime'])) { echo $error['millesime']; } ?>
                    		</span>
@@ -57,9 +57,9 @@
 
 			<div class="row">
 				<div class="col-md-4">
-					<div class="form-group">
+					<div class="form-group <?php if (isset($error['cepage'])) { echo 'has-error'; } ?>">
 						<label for="cepage">Cépage</label>
-						<input type="text" name="cepage" class="form-control" value="<?= $cepage; ?>">
+						<input type="text" name="cepage" id="cepage" class="form-control" value="<?= $cepage; ?>" data-min="3" data-max="16" required="required">
 						<span class="help-block" <?php if (empty($error['cepage'])) { echo 'style="display: none"'; } ?>>
                         <?php if (isset($error['cepage'])) { echo $error['cepage']; } ?>
                    		</span>
@@ -67,9 +67,9 @@
 				</div>
 
 				<div class="col-md-4 ">
-					<div class="form-group">
+					<div class="form-group <?php if (isset($error['stock'])) { echo 'has-error'; } ?>">
 						<label for="stock">Stock</label>
-						<input type="text" name="stock" class="form-control" value="<?= $stock; ?>">
+						<input type="text" name="stock" id="stock" class="form-control" value="<?= $stock; ?>" data-min="2" required="required">
 						<span class="help-block" <?php if (empty($error['stock'])) { echo 'style="display: none"'; } ?>>
                         <?php if (isset($error['stock'])) { echo $error['stock']; } ?>
                    		</span>
@@ -78,7 +78,7 @@
 
 				<div class="col-md-4">
 					<label for="bio">Vin bio</label>
-					<input type="checkbox" name="bio">
+					<input type="checkbox" name="bio" id="bio">
 				</div>
 			</div>
 
@@ -90,6 +90,7 @@
 			</div>
 
 			<div>
+			<!-- INPUT HIDDEN TEMPORAIRE -->
 				<input type="hidden" name="winemaker_id" value="e3e6747f8834d39724bbde3b5b133996">
 				<input type="submit" class="btn btn-default" value="Ajouter">
 			</div>
