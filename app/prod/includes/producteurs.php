bb01fc472dc54e3f5ccf5d81546b0fe2f76d290b
IRONBAN <heramban_minatchy@hotmail.com>
2017-02-10 18:27:44 +0100

    Maj

--- app/prod/includes/producteurs.php
+++ app/prod/includes/producteurs.php
@@ -30,11 +30,11 @@
 	                                <div class="item" data-slide-number="5">
 	                                <img src="assets/img/yann.jpg" width="100%"></div>
 
-	                                <div class="item" data-slide-number="5">
-	                                <img src="assets/img/yann.jpg" width="100%"></div>
+	                                <div class="item" data-slide-number="3">
+	                                <img src="assets/img/romain.jpg" width="100%"></div>
 
-	                                <div class="item" data-slide-number="5">
-	                                <img src="assets/img/yann.jpg" width="100%"></div>
+	                                <div class="item" data-slide-number="2">
+	                                <img src="assets/img/thomas.jpg" width="100%"></div>
 
 	                            </div><!-- Carousel nav -->
 	                            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
@@ -50,7 +50,7 @@
 
 	                    <div id="slide-content" style="display: none;">
 	                        <div id="slide-content-0">
-	                            <h2>Pierre Vaneste</h2>
+	                            <h3><strong>Pierre Vaneste</strong></h3>
 	                            <h4>Producteur à Convention et sur le dancefloor</h4>
 	                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -70,11 +70,11 @@
 	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	                            quis nostrud exercita</p>
-	                            <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+	                            <p class="sub-text">October 24 2014 - <a href="#">lire la suite...</a></p>
 	                        </div>
 
 	                        <div id="slide-content-1">
-	                            <h2>Héramban Minatchy</h2>
+	                            <h3><strong>Héramban Minatchy</strong></h3>
 	                            <h4>Producteur à La Réunion</h4>
 	                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -95,11 +95,11 @@
 	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	                            quis nostrud exercita</p>
-	                            <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+	                            <p class="sub-text">October 24 2014 - <a href="#">lire la suite...</a></p>
 	                        </div>
 
 	                        <div id="slide-content-2">
-	                            <h2>Thomas Mion</h2>
+	                            <h3><strong>Thomas Mion</strong></h3>
 	                            <h4>Producteur à Vincennes</h4>
 	                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -108,7 +108,7 @@
 	                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-	                            <blockquote> <em>"Votre manque de foie me consterne..."</em>
+	                            <blockquote> <em>"Votre manque de foi me consterne..."</em>
 	                           <small>Thomas Mion</small></blockquote></p>
 
 	                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -121,11 +121,11 @@
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	                            quis nostrud exercita</p>
 
-	                            <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+	                            <p class="sub-text">October 24 2014 - <a href="#">lire la suite...</a></p>
 	                        </div>
 
 	                        <div id="slide-content-3">
-	                            <h2>Romain Hamel</h2>
+	                            <h3><strong>Romain Hamel</strong></h3>
 	                            <h4>Producteur chez lui</h4>
 	                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -146,11 +146,11 @@
 	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	                            quis nostrud exercita</p>
-	                            <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+	                            <p class="sub-text">October 24 2014 - <a href="#">lire la suite...</a></p>
 	                        </div>
 
 	                        <div id="slide-content-4">
-	                            <h2>Hwa Seon Lecouey</h2>
+	                            <h3><strong>Hwa Seon Lecouey</strong></h3>
 	                            <h4>Producteur à Séoul</h4>
 	                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -171,11 +171,11 @@
 	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	                            quis nostrud exercita</p>
-	                            <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+	                            <p class="sub-text">October 24 2014 - <a href="#">lire la suite...</a></p>
 	                        </div>
 
 	                        <div id="slide-content-5">
-	                            <h2>Yann Le Merrer</h2>
+	                            <h3><strong>Yann Le Merrer</strong></h3>
 	                            <h4>Producteur à la Retraite</h4>
 	                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -196,7 +196,7 @@
 	                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	                            quis nostrud exercita</p>
-	                            <p class="sub-text">October 24 2014 - <a href="#">Read more</a></p>
+	                            <p class="sub-text">October 24 2014 - <a href="#">lire la suite...</a></p>
 	                        </div>
 	                    </div>
 	                </div>
@@ -204,8 +204,9 @@
 	        </div><!--/Slider-->
 
 	        <div class="row hidden-xs" id="slider-thumbs">
+
 	            <!-- Bottom switcher of slider -->
-	            <ul class="hide-bullets">
+	            <ul class="grayscale" class="hide-bullets">
 	                <li class="col-sm-2">
 	                    <a class="thumbnail" id="carousel-selector-0"><img src="assets/img/pierre.jpg" ></a>
 	                </li>
@@ -231,11 +232,11 @@
 	                </li>
 
 	                <li class="col-sm-2">
-	                    <a class="thumbnail" id="carousel-selector-5"><img src="assets/img/yann.jpg" ></a>
+	                    <a class="thumbnail" id="carousel-selector-2"><img src="assets/img/thomas.jpg" ></a>
 	                </li>
 
 	                <li class="col-sm-2">
-	                    <a class="thumbnail" id="carousel-selector-5"><img src="assets/img/yann.jpg" ></a>
+	                    <a class="thumbnail" id="carousel-selector-3"><img src="assets/img/romain.jpg" ></a>
 	                </li>
 	            </ul>
 	        </div>
