fde9615692b598f6402af74ec6d874b6b7722e14
IRONBAN <heramban_minatchy@hotmail.com>
2017-02-11 15:34:39 +0100

    MAJ

--- app/Views/dashboard/product.php
+++ app/Views/dashboard/product.php
@@ -3,32 +3,65 @@
 
 <section>
 
+<!-- <div class="container">
+	<div class="row">
+		<div class="cadre">
+			<p>Nom : <?= $product['name'] ?></p>
+			<p>Cépage : <?= $product['cepage'] ?></p>
+			<p>Millésime : <?= $product['millesime'] ?></p>
+			<p>Région : <?= $product['region'] ?></p>
+			<p>Producteur : <?= $product['winemaker_id'] ?></p>
+			<p>Prix : <?= $product['price'] ?></p>
+			<p>Stock : <?= $product['stock'] ?></p>
+			<p>Reviews : <?= $product['reviews_id'] ?></p>
+		</div>
+	</div>
+</div> -->
 
-<div id="productDetails">
-
-<table class="table table-striped">
-
-  <tbody>
-    <tr>
+<div class="container"> 
+<div class="produitImg">
+	<img src="<?= $this->assetUrl('img/vinBouteille.jpg') ?>">
+</div>
+  <table class="table table-striped">
+  
+    <tbody>
+      <tr>
+         <td><strong>Nom :</strong></td>
+	      <td><?= $product['name'] ?></td>
+      </tr>
+  
+  
+      <tr>
+        <td><strong>Cépage :</strong></td>
+	      <td><?= $product['cepage'] ?></td>
+      </tr>
+      <tr>
+        <td><strong>Millésime :</strong></td>
+	      <td><?= $product['millesime'] ?></td>
+      </tr>
+      <tr>
+        <td><strong>Région :</strong></td>
+	      <td><?= $product['region'] ?></td>
+      </tr>
+      <tr>
+        <td><strong>Producteur :</strong></td>
+	      <td><?= $product['winemaker_id'] ?></td>
+      </tr>
+      <tr>
+        <td><strong>Prix :</strong></td>
+	      <td><?= $product['price'] ?></td>
+      </tr>
+      <tr>
+        <td><strong>Stock :</strong></td>
+	      <td><?= $product['stock'] ?></td>
+      </tr>
+      <tr>
+        <td><strong>Reviews :</strong></td>
+	      <td><?= $product['reviews_id'] ?></td>
+      </tr>
       
-      <td>Mark</td>
-      <td>Otto</td>
-
-    </tr>
-    <tr>
-      
-      <td>Jacob</td>
-      <td>Thornton</td>
-
-    </tr>
-    <tr>
-      
-      <td>Larry</td>
-      <td>the Bird</td>
- 
-    </tr>
-  </tbody>
-</table>
+    </tbody>
+  </table>
 </div>
 
 
