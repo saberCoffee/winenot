6aa2b77011dcebf59a52216c939a97fa5b103162
IRONBAN <heramban_minatchy@hotmail.com>
2017-02-13 21:10:53 +0100

    JAM CSS WINEMAKER OF THE MONTH

--- app/Views/admin/products.php
+++ app/Views/admin/products.php
@@ -6,35 +6,46 @@
         <div class="alert alert-success" role="alert"><?= $_COOKIE['successMsg'] ?></div>
     </section>
 <?php } ?>
+<div class="Wineofmonth">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-10">
+                <form class="form" id="winesOfTheMonth" action="<?= $this->url('admin_products') ?>" method="post">
+                    <table border="1" class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nom</th>
+                                <th>Vin du mois</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
 
-<form class="form" id="winesOfTheMonth" action="<?= $this->url('admin_products') ?>" method="post">
-    <table border="1" class="table table-striped">
-        <thead>
-            <tr>
-                <th>Nom</th>
-                <th>Vin du mois</th>
-                <th>Description</th>
-            </tr>
-        </thead>
+                        <tbody>
+                            <?php foreach ($products as $product) : ?>
+                            <tr>
+                                <td><label for="<?= $product['id'] ?>"><?= $product['name'];?></label></td>
+                                <td><input type="checkbox" class="wine_of_the_month" name="wine_of_the_month[]" id="<?= $product['id'] ?>" value="<?= $product['id'] ?>" <?php if ($product['checked'] == 1) { echo 'checked="checked"'; } ?> /></td>
+                                <td><?= $product['description'];?></td>
+                            </tr>
+                            <?php endforeach;?>
+                        </tbody>
+                    </table>
 
-        <tbody>
-            <?php foreach ($products as $product) : ?>
-            <tr>
-                <td><label for="<?= $product['id'] ?>"><?= $product['name'];?></label></td>
-                <td><input type="checkbox" class="wine_of_the_month" name="wine_of_the_month[]" id="<?= $product['id'] ?>" value="<?= $product['id'] ?>" <?php if ($product['checked'] == 1) { echo 'checked="checked"'; } ?> /></td>
-                <td><?= $product['description'];?></td>
-            </tr>
-            <?php endforeach;?>
-        </tbody>
-    </table>
-
-    <span class="help-block" <?php if (empty($error)) { echo 'style="display: none"'; } ?>>
-        <?php if (isset($error)) { echo $error; } ?>
-    </span>
-
-    <input type="submit" name="submit" value="Valider" />
-</form>
-
+                    <span class="help-block" <?php if (empty($error)) { echo 'style="display: none"'; } ?>>
+                        <?php if (isset($error)) { echo $error; } ?>
+                    </span>
+                <div class="row">
+                        <div class="col-lg-12">
+                            <div class="BtnSubmit">
+                                <input type="submit" name="submit" value="Valider" class="btn btn-default"/>
+                            </div>
+                        </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <?php $this->stop('main_content') ?>
 
 <?php $this->start('js') ?>
