773d294ddcd4ed46c7792445746b70dfcdf17554
IRONBAN <heramban_minatchy@hotmail.com>
2017-02-13 17:46:03 +0100

    jam TXT

--- app/Views/dashboard/home.php
+++ app/Views/dashboard/home.php
@@ -7,7 +7,8 @@
     <img src="<?= $this->assetUrl('img/dashboard/home-winemakers.jpg') ?>" alt="Producteurs" class="img-responsive" />
 
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        La diversité du climat français associée à une infinité de terroirs, géologiquement très variés, sont particulièrement propices à la culture de la vigne et à la production de grands vins. Le vignoble français produit 3240 vins différents sur 80 départements et 16 grands vignobles. 
+        Voici la liste de nos vignobles. Cliquez sur les liens pour découvrir ce qui fait la spécificité de chacun d'eux à travers leurs terroirs, cépages, histoires et cartes des vins interactives...
     </p>
 
     <p>
@@ -21,7 +22,8 @@
     <img src="<?= $this->assetUrl('img/dashboard/home-products.jpg') ?>" alt="Producteurs" class="img-responsive" />
 
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Parmi tous ces vignerons, certains révèlent des vins très au-dessus de la moyenne. On sent à travers leurs vins une implication, une abnégation et une passion que l'on ne parvient pas à retrouver ailleurs. Oui, ce sont des jardinier(e)s de la vigne, finalement il n'y a jamais de hasard dans le vin...
+        Découvrez tout les vins de nos producteurs.
     </p>
 
     <p>
