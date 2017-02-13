d261b871320044a2d182f853d66a020dae0b170e
Tadeca <th.mion@gmail.com>
2017-02-13 21:16:01 +0100

    Bidouille products

--- app/Views/admin/products.php
+++ app/Views/admin/products.php
@@ -10,7 +10,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-10">
-                <form class="form" id="winesOfTheMonth" action="<?= $this->url('admin_products') ?>" method="post">
+                <form class="form" id="winesOfTheMonth-form" action="<?= $this->url('admin_products') ?>" method="post">
                     <table border="1" class="table table-striped">
                         <thead>
                             <tr>
@@ -20,7 +20,6 @@
                             </tr>
                         </thead>
 
-<<<<<<< HEAD
                         <tbody>
                             <?php foreach ($products as $product) : ?>
                             <tr>
@@ -31,46 +30,12 @@
                             <?php endforeach;?>
                         </tbody>
                     </table>
-=======
-<form class="form" id="winesOfTheMonth-form" action="<?= $this->url('admin_products') ?>" method="post">
-    <table border="1" class="table table-striped">
-        <thead>
-            <tr>
-                <th>Nom</th>
-                <th>Vin du mois</th>
-                <th>Description</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            <?php foreach ($products as $product) : ?>
-            <tr>
-                <td><label for="<?= $product['id'] ?>"><?= $product['name'];?></label></td>
-                <td><input type="checkbox" class="wine_of_the_month" name="wine_of_the_month[]" id="<?= $product['id'] ?>" value="<?= $product['id'] ?>" <?php if ($product['checked'] == 1) { echo 'checked="checked"'; } ?> /></td>
-                <td><?= $product['description'];?></td>
-            </tr>
-            <?php endforeach;?>
-        </tbody>
-    </table>
-
-    <span class="help-block" <?php if (empty($error)) { echo 'style="display: none"'; } ?>>
-        <?php if (isset($error)) { echo $error; } ?>
-    </span>
-
-    <input type="submit" name="submit" value="Valider" />
-</form>
->>>>>>> 1c5654645b46c83245074109680b4d8bdce83e5c
 
                     <span class="help-block" <?php if (empty($error)) { echo 'style="display: none"'; } ?>>
                         <?php if (isset($error)) { echo $error; } ?>
                     </span>
-                <div class="row">
-                        <div class="col-lg-12">
-                            <div class="BtnSubmit">
-                                <input type="submit" name="submit" value="Valider" class="btn btn-default"/>
-                            </div>
-                        </div>
-                </div>
+
+                    <input type="submit" name="submit" value="Valider" />
                 </form>
             </div>
         </div>
